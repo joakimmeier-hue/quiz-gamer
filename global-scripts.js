1cb17bcda78361ab0f983350ab7e44e58f469bf9
joakimmeier-hue <joakim.meier@gmail.com>
2026-09-11 02:40:15 +0200

    Fire "321-go

--- global-scripts.js
+++ global-scripts.js
@@ -538,13 +538,13 @@ Webflow.push(function() {
     setTimeout(function() {
       if (window.FinalTimeSecs > 0 || window.FinalTimeStr !== "00:00") return;
       emitWebflowEvent("321-go");
-    }, 1000); 
+    }, 1500); 
 
     // 2. Start Lottie animation ("start-stopwatch")
     setTimeout(function() {
       if (window.FinalTimeSecs > 0 || window.FinalTimeStr !== "00:00") return;
       emitWebflowEvent("start-stopwatch");
-    }, 2100); 
+    }, 2600); 
 
     // 3. Start running numbers in the timer
     setTimeout(function() {
