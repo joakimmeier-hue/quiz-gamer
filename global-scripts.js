fd83b96481a84224ab2b0e9df53df1da1206baaa
joakimmeier-hue <joakim.meier@gmail.com>
2026-09-24 00:36:16 +0200

    // 2 COMPONENT .dropdown-gamelevel

--- global-scripts.js
+++ global-scripts.js
@@ -1685,8 +1685,8 @@ const HOVER_SCALE_CLASSES = [
     'profile-pic-option',
     'current-profile-pic',
     'login-modal-btn',
-    'dropdown-toggle-lvl',
-    'game-level-option', 
+    /* 'dropdown-toggle-lvl',
+    'game-level-option',  */
     'merge-google-btn',
     'cancel-merge-btn',
     'eam-submit-btn',
@@ -1721,8 +1721,8 @@ const PRESS_SCALE_CLASSES = [
     'profile-pic-option',
     'current-profile-pic',
     'login-modal-btn',
-    'dropdown-toggle-lvl',
-    'game-level-option',    
+   /*  'dropdown-toggle-lvl',
+    'game-level-option',  */   
     'merge-google-btn',
     'cancel-merge-btn',
     'eam-submit-btn',
